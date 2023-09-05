74dc6d5bc70ea8387d2c7fe8b5b441e8efb211ba
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-05 18:23:47 +0400

    Initial commit

---
+++ Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿var dictionary = new Dictionary<string, string>
+{
+    { "Монитор", "Устройство вывода" },
+    { "Клавиатура", "Устройство ввода" }
+};
+
+Console.Write("Введите слово: ");
+var word = Console.ReadLine()!;
+
+Console.WriteLine(dictionary.TryGetValue(word, out var value));
+if (dictionary.ContainsKey(word!))
+{
+    Console.WriteLine($"Значение слова {word}: {dictionary[value!]}");
+}
+else
+{
+    Console.WriteLine($"Слово {word} не содержится в словаре");
+}
+
+/*? $"Значение слова {word}: {value}"
+: $"Слово {word} отсутствует в словаре");*/
